a4cdecd289e3f2b97002c27a744023b5e0baf19c
STEIN197 <nil20122013@gmail.com>
2018-07-07 16:57:58 +0500

    Added useful css rules

--- index.php
+++ index.php
@@ -5,20 +5,25 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 	<link rel="stylesheet" href="/css/bootstrap.min.css"/>
 	<link rel="stylesheet" href="/css/bootstrap-grid.min.css"/>
-	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700" rel="stylesheet"/>
 	<!--[if IE]>
 		<link href="/css/bootstrap-ie9.min.css" rel="stylesheet">
 		<script src="https://cdn.jsdelivr.net/g/html5shiv@3.7.3"></script>
 	<![endif]-->
 	<link rel="stylesheet" href="/css/slick.min.css"/>
 	<link rel="stylesheet" href="/css/magnific-popup.min.css"/>
-	<link rel="stylesheet" href="/css/styles.min.css?v=<?= filemtime("{$_SERVER['DOCUMENT_ROOT']}/css/styles.min.css") ?>"/>
+	<link rel="stylesheet" href="/css/template.min.css?v=<?= filemtime("{$_SERVER['DOCUMENT_ROOT']}/css/template.min.css") ?>"/>
 	<title></title>
 	<meta name="description" content=""/>
 	<meta name="keywords" content=""/>
 </head>
 <body>
+	<p class="font-proxima fw-regular fs-md-58">Proxima</p>
+	<p class="font-proxima fw-bold fs-md-58">Proxima</p>
+	<p class="font-proxima fw-bold-x fs-md-58 lh-0 bg-c-black color-white">Proxima</p>
+	<p class="font-proxima fw-black fs-md-58">Proxima</p>
+	<div class="full-w h-150 h-md-130 h-lg-120 h-xl-100 pd-15 bg-c-gray pd-x-md-30 pd-y-lg-100 pd-b-xl-75">
 	
+	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="/js/bootstrap.min.js"></script>
@@ -27,6 +32,6 @@
 	<script src="/js/jquery.inputmask.bundle.min.js"></script>
 	<script src="/js/jquery.placeholder.min.js"></script>
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBl2Ifc-eVEmfjSPG-DaA5pih_79CJkvyU&#038;ver=4.8.2"></script>
-	<script src="/js/app.js?v=<?= filemtime("{$_SERVER['DOCUMENT_ROOT']}/js/app.js") ?>"></script>
+	<script src="/js/template.js?v=<?= filemtime("{$_SERVER['DOCUMENT_ROOT']}/js/template.js") ?>"></script>
 </body>
 </html>
