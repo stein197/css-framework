6984040bed4bf7dae7a12c101fc3c8985f60bcee
Nail <nil20122013@gmail.com>
2020-03-13 13:44:31 +0500

    Made .\build.bat work properly

--- build.bat
+++ build.bat
@@ -1,8 +1,11 @@
 @ECHO OFF
-ECHO Clearing dist folter...
-del /q dist\*
+ECHO Clearing .\dist folder...
+CALL DEL /q dist\*
+
+ECHO Compiling SCSS file...
+CALL scss --style compressed css\template.scss dist\template.min.css
+
 ECHO Minifying JS file...
-uglifyjs -o dist\template.min.js js\template.js
-ECHO Compiling CSS file...
-scss --style compressed css\template.scss dist\template.min.css
-ECHO Finished
+CALL uglifyjs -o dist\template.min.js js\template.js
+
+ECHO Complete
