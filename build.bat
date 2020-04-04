b9923b0fd05c0738b4254958b1c08915cbdf0e80
STEIN197 <nil20122013@gmail.com>
2020-04-04 15:53:06 +0500

    Now checks on command existance are present

--- build.bat
+++ build.bat
@@ -1,11 +1,29 @@
 @ECHO OFF
-ECHO Clearing .\dist folder...
-CALL DEL /q dist\*
+SETLOCAL
+SET folder=dist
+SET msg=Complete
+
+ECHO Clearing "%folder%" folder...
+CALL DEL /q .\%folder%\*
+
+WHERE /Q scss
+IF %ERRORLEVEL% NEQ 0 (
+	SET msg=SCSS/SASS is not installed. Install and add it to the %%PATH%% variable
+	GOTO END
+)
 
 ECHO Compiling SCSS file...
-CALL scss --no-cache --style compressed css\template.scss dist\template.min.css
+CALL scss --no-cache --style compressed .\css\template.scss .\%folder%\template.min.css
+
+WHERE /Q uglifyjs
+IF %ERRORLEVEL% NEQ 0 (
+	SET msg=uglifyjs for Node.js is not installed. Install and add it to the %%PATH%% variable
+	GOTO END
+)
 
 ECHO Minifying JS file...
-CALL uglifyjs -o dist\template.min.js js\template.js
+CALL uglifyjs -o .\%folder%\template.min.js js\template.js
 
-ECHO Complete
+:END
+ECHO %msg%
+EXIT /B %ERRORLEVEL%
