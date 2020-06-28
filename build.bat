631eef2b98c85191c2dab048e9d1f4031907d6f0
STEIN197 <nil20122013@gmail.com>
2020-06-28 15:33:47 +0500

    Added Unix-platform build, removed unused JS

--- build.bat
+++ build.bat
@@ -15,15 +15,7 @@ IF %ERRORLEVEL% NEQ 0 (
 ECHO Compiling SCSS file...
 CALL scss --no-cache --style compressed .\css\template.scss .\%folder%\template.min.css
 
-WHERE /Q uglifyjs
-IF %ERRORLEVEL% NEQ 0 (
-	SET msg=uglifyjs for Node.js is not installed. Install and add it to the %%PATH%% variable
-	GOTO END
-)
-
-ECHO Minifying JS file...
-CALL uglifyjs -o .\%folder%\template.min.js js\template.js
-
 :END
 ECHO %msg%
 EXIT /B %ERRORLEVEL%
+
